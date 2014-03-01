c28e6cddd20d647ec6ba47bf9058dbaa690b0c7d
rohit12 <rohit.shinde12194@gmail.com>
2014-03-01 23:33:03 +0530

    Added Searching

--- BinaryTree.py
+++ BinaryTree.py
@@ -13,18 +13,31 @@ class Node:
 			print self.right.display()
 
 	def display_preorder(self):
-                print self.data
-                if self.left:
-                        print self.left.display()
-                if self.right:
-                        print self.right.display()
+        print self.data
+        if self.left:
+        	print self.left.display()
+        if self.right:
+            print self.right.display()
 	
 	def display_postorder(self):
-                print self.data
-                if self.left:
-                        print self.left.display()
-                if self.right:
-                        print self.right.display()
+        if self.left:
+            print self.left.display()
+        if self.right:
+            print self.right.display()
+
+         print self.data
+
+    def lookup(self,data,parent=None):
+    	if data < self.data:
+    		if self.left is None:
+    			return None, None
+    		return self.left.lookup(data,self)
+    	elif data > self.data :
+    		if self.right is None:
+    			return None, None
+    		return self.right.lookup(data,self)
+    	else:
+    		return self, parent
 
 root=Node(8)
 root.display_inorder()
