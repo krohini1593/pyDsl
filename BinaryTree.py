84226661a8f004276b7a5c561822c54a1f5a50b6
Bhushan <bhushan.s.94@gmail.com>
2014-03-01 23:05:18 +0530

    Binary Tree traversal added

--- BinaryTree.py
+++ BinaryTree.py
@@ -5,13 +5,27 @@ class Node:
 		self.right=None;
 		self.data=data;
 
-	def display(self):
+	def display_inorder(self):
 		if self.left:
 			print self.left.display()
 		print self.data
 		if self.right:
 			print self.right.display()
 
+	def display_preorder(self):
+                print self.data
+                if self.left:
+                        print self.left.display()
+                if self.right:
+                        print self.right.display()
+	
+	def display_postorder(self):
+                print self.data
+                if self.left:
+                        print self.left.display()
+                if self.right:
+                        print self.right.display()
 
 root=Node(8)
-root.display()
+root.display_inorder()
+
