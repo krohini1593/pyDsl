6f4da6a232efc4c28770fdd2afb5aeb50594c2ea
Bhushan <bhushan.s.94@gmail.com>
2014-03-02 23:03:53 +0530

    Red Black Tree and tree Folder Created

---
+++ tree/RedBlackTree.py
@@ -0,0 +1,11 @@
+class RedBlackTree:
+	def __init__(self,data):
+		self.left=None
+		self.right=None
+		self.data=data
+		self.color="BLACK"
+
+	def insert(data):
+		newNode=RedBlackTree(data)
+		newNode.color="RED"
+
