c413f3627e8462b3367779fe8dd02288e3ea0f07
bhushan23 <bhushan.s.94@gmail.com>
2014-03-02 22:43:08 +0530

    Binary Tree indented
    Binary Tree with indentation

--- BinaryTree.py
+++ BinaryTree.py
@@ -13,30 +13,29 @@ class BinarySearchTree:
 			print self.right.display()
 
 	def display_preorder(self):
-        print self.data
-        if self.left:
-            print self.left.display()
-        if self.right:
-            print self.right.display()
+        	print self.data
+        	if self.left:
+                	print self.left.display()
+        	if self.right:
+                	print self.right.display()
 	
 	def display_postorder(self):
-        if self.left:
-            print self.left.display()
-        if self.right:
-            print self.right.display()
+                if self.left:
+           		 print self.left.display()
+       		if self.right:
+           		 print self.right.display()
+	        print self.data
 
-        print self.data
-
-    def lookup(self,data,parent=None):
-    	if data < self.data:
-    		if self.left is None:
-    			return None, None
-    		return self.left.lookup(data,self)
-    	elif data > self.data :
+	def lookup(self,data,parent=None):
+    		 if data < self.data:
+    			if self.left is None:
+    				return None, None
+    	         return self.left.lookup(data,self)
+    	 elif data > self.data :
     		if self.right is None:
     			return None, None
     		return self.right.lookup(data,self)
-    	else:
+    	 else:
     		return self, parent
 
 root=BinarySearchTree(8)
